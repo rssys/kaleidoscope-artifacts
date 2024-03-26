52338dcfef8db8dffb22d542680e725037b16ff5
Tapti Palit <tpalit@purdue.edu>
2024-03-26 01:44:46 +0000

    update

--- llvm-setup.sh
+++ llvm-setup.sh
@@ -3,11 +3,12 @@
 
 # compile the gold linker
 
+set -x
 apt-get -y install libgmp-dev libmpfr-dev
 
-ssh-keygen -b 2048 -t rsa -f /tmp/sshkey -q -N ""
+yes | ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-
+ssh -o StrictHostKeyChecking=no user@github.com
 
 git clone --depth 1 git://sourceware.org/git/binutils-gdb.git binutils
 mkdir gold-build
@@ -27,13 +28,14 @@ cd llvm12
 
 export BINUTILS_INC_DIR="$(realpath ../binutils/include)"
 cmake -S llvm -B debug-build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug \
--DLLVM_ENABLE_DUMP=ON  -DLLVM_ENABLE_PROJECTS="compiler-rt;clang;lld" \
--DLLVM_BINUTILS_INCDIR=$BINUTILS_INC_DIR  -DCMAKE_INSTALL_PREFIX=/usr/
+	-DLLVM_ENABLE_DUMP=ON  -DLLVM_ENABLE_PROJECTS="compiler-rt;clang;lld" \
+	-DLLVM_BINUTILS_INCDIR=$BINUTILS_INC_DIR  -DCMAKE_INSTALL_PREFIX=/usr/
 cd debug-build
 make -j8
 cd ../..
 
-echo "export PATH=\"$(realpath ./llvm12/debug-build/bin):\$PATH\"" >> ~/.bashrc
+echo "export PATH=\"$(realpath ./llvm12/debug-build/bin):\$PATH\"" >>
+~/.bashrc
 echo "export LLVM_DIR=\"$(realpath ./llvm12/debug-build/bin)\"" >> ~/.bashrc
 LLVM_DIR=$(realpath ./llvm12/debug-build/bin)
 echo "export LLVM_HOME=$LLVM_DIR" >> ~/.bashrc
